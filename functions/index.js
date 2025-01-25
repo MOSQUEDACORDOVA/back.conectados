2c3c61ef36493f47f0e51eddc1274d6fba8840b7
MOSQUEDACORDOVA <69749643+MOSQUEDACORDOVA@users.noreply.github.com>
2025-01-25 09:13:26 -0400

    Initial and test

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
 const functions = require("firebase-functions");
 
-// Function HTTP basic
+// Primera función: Hola Mundo
 exports.helloWorld = functions.https.onRequest((req, res) => {
-  res.status(200).send("¡Hola, Firebase está funcionando!");
+  res.status(200).send("¡Hola, esta es la función helloWorld!");
+});
+// Segunda función: Adiós Mundo
+exports.goodbyeWorld = functions.https.onRequest((req, res) => {
+  res.status(200).send("¡Adiós, esta es la función goodbyeWorld!");
 });
